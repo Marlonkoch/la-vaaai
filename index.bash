2f365c53500df9276960aa5bb7b302f1634d0465
Marlonkoch <160977577+Marlonkoch@users.noreply.github.com>
2024-11-26 05:11:17 -0300

    Add files via upload

---
+++ index.bash
@@ -0,0 +1,15 @@
+/matrizes-energeticas
+    ├── /css
+    │    └── style.css
+    ├── /js
+    │    └── script.js
+    ├── /images
+    │    ├── energia-solar.jpg
+    │    ├── energia-eolica.jpg
+    │    └── energia-hidreletrica.jpg
+    ├── index.html
+    ├── fontes-energia.html
+    ├── impactos-ambientais.html
+    ├── tecnologias-emergentes.html
+    ├── politicas-energeticas.html
+    └── contato.html
